724346113525c09bc32082340c2704b3eeed22be
Alpha Kamara <kamaraalpha013@gmail.com>
2025-05-09 10:39:41 +0000

    Made some changes to the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="hidden md:flex  md:gap-10 ">
-      <a href="#home" className="font-bold text-2xl hover:text-gray-600">
+    <nav className="hidden md:flex  md:gap-10 text-black hover:text-gray-300">
+      <a href="#home" className="font-bold text-2xl hover:text-black">
         Home
       </a>
-      <a href="#about" className="font-bold text-2xl hover:text-gray-600">
+      <a href="#about" className="font-bold text-2xl hover:text-black">
         About
       </a>
-      <a href="#service" className="font-bold text-2xl hover:text-gray-600">
+      <a href="#service" className="font-bold text-2xl hover:text-black">
         Services
       </a>
-      <a href="#news" className="font-bold text-2xl hover:text-gray-600">
+      <a href="#news" className="font-bold text-2xl hover:text-black">
         News
       </a>
     </nav>
